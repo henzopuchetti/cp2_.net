b13bf3ee025a31b9cef45ae248d6ea9df192fa0d
henzo <84385799+henzopuchetti@users.noreply.github.com>
2025-05-12 21:31:24 -0300

    Add files via upload

---
+++ LembreteMedicamentos.Domain/Entities/Idoso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LembreteMedicamentos.Domain.Entities;
+public class Idoso
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public ICollection<Lembrete> Lembretes { get; set; }
+}
+
---
+++ LembreteMedicamentos.Domain/Entities/Lembrete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LembreteMedicamentos.Domain.Entities;
+public class Lembrete
+{
+    public int Id { get; set; }
+    public DateTime Horario { get; set; }
+    public string Observacoes { get; set; }
+
+    public int IdosoId { get; set; }
+    public Idoso Idoso { get; set; }
+
+    public int MedicamentoId { get; set; }
+    public Medicamento Medicamento { get; set; }
+}
+
